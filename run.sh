e6170bf90554fe1bbb2a084c05c055ab9d9d0c4c
Coli-co <awestlifeblue@gmail.com>
2023-03-28 15:14:00 +0800

    add register username logic

--- run.sh
+++ run.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-echo "Hello World"
+# different operation by user
+operate="$1" # operation variable
+name="$2" # username variable
+# check username from upper to lowercase
+username=${name,,}
+
+# Check if the user exists
+if [ "$operate" == "register" ];
+then
+  if [ "$username" != "user1" ];
+  then
+    echo "Error - user already existing"
+  else
+    echo 'Success'
+  fi
+fi
